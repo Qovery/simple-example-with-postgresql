e8d08d6e408f6e1a33c0b93fadd0f8ebe01157ba
Romaric Philogene <evoxmusic@gmail.com>
2019-12-26 00:41:37 +0100

    update: build.gradle.kts (test)

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-
     runtimeOnly("com.h2database:h2:1.4.200")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
